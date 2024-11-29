6896d664be2c2163bf309ff6ae89ee0785fbca98
Vaibhav Tiwari <tiwarijivaibhav@gmail.com>
2024-11-29 06:09:27 +0530

    cleanup

--- frontend/src/Components/DataOverlay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DataOverlay = () => {
-  return (
-    <div>DataOverlay</div>
-  )
-}
-
-export default DataOverlay
--- frontend/src/Components/Visualization3D.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Visualization3D = () => {
-  return (
-    <div>Visualization3D</div>
-  )
-}
-
-export default Visualization3D
